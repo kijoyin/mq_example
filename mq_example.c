68162547e6ff43f956022dd0217c0af3aacbcf5e
Kiran Joy <kijoyin@gmail.com>
2018-09-14 23:47:35 +0000

    sad

--- mq_example.c
+++ mq_example.c
@@ -23,6 +23,8 @@
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
+#include <bluetooth/sdp.h>
+#include <bluetooth/sdp_lib.h>
 
 #define CAR_SPEED "/car_speed"
 #define CAR_CMD "/car_cmd"
@@ -42,6 +44,9 @@ static float speedKmPerHour;
 
 void calculateSpeed_main(void);
 void receiveCarCmd_main(void);
+sdp_session_t *register_service();
+int init_server();
+
 
 void sig_handler(int signum) {
     if (signum != SIGINT) {
@@ -120,48 +125,6 @@ void receiveCarCmd_main(void) {
 
     printf("Car cmd started started. Execution period = %d uSecs\n",\
                                            exec_period_usecs);
-    struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
-    char buf[1024] = { 0 };
-    int s, client, bytes_read;
-    socklen_t opt = sizeof(rem_addr);
-
-    // allocate socket
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-
-    // bind socket to port 1 of the first available 
-    // local bluetooth adapter
-    loc_addr.rc_family = AF_BLUETOOTH;
-    loc_addr.rc_bdaddr = *BDADDR_ANY;
-    loc_addr.rc_channel = (uint8_t) 1;
-    if(bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr)) < 0)
-    {
-		perror("bind");
-        exit(1);
-	}
-
-    // put socket into listening mode
-    listen(s, 1);
-
-    // accept one connection
-    printf("Waiting for connection!\n");
-    while(client = accept(s, (struct sockaddr *)&rem_addr, &opt))
-    {
-      printf("Got a connection attempt!\n");
-    }
-
-    ba2str( &rem_addr.rc_bdaddr, buf );
-    fprintf(stderr, "accepted connection from %s\n", buf);
-    memset(buf, 0, sizeof(buf));
-
-    // read data from the client
-    bytes_read = read(client, buf, sizeof(buf));
-    if( bytes_read > 0 ) {
-        printf("received [%s]\n", buf);
-    }
-
-    // close connection
-    close(client);
-    close(s);
     while(1) {
         status = mq_send(car_cmd, (const char*)&counter, sizeof(counter), 1);
         ASSERT(status != -1);
@@ -178,6 +141,7 @@ void calculateSpeed_main(void) {
 
     printf("Car thread started. Execution period = %d uSecs\n",\
                                            exec_period_usecs);
+    init_server();
     while(1) {
         status = mq_receive(car_mq, (char*)&recv_counter, \
                             sizeof(recv_counter), NULL);
@@ -189,4 +153,132 @@ void calculateSpeed_main(void) {
         usleep(exec_period_usecs);
     }
 }
+sdp_session_t *register_service() {
+
+	uint32_t service_uuid_int[] = { 0, 0, 0, 0xABCD };
+    uint8_t rfcomm_channel = 11;
+    const char *service_name = "Roto-Rooter Data Router";
+    const char *service_dsc = "An experimental plumbing router";
+    const char *service_prov = "Roto-Rooter";
+
+    uuid_t root_uuid, l2cap_uuid, rfcomm_uuid, svc_uuid;
+    sdp_list_t *l2cap_list = 0, 
+               *rfcomm_list = 0,
+               *root_list = 0,
+               *proto_list = 0, 
+               *access_proto_list = 0;
+    sdp_data_t *channel = 0, *psm = 0;
+
+    sdp_record_t *record = sdp_record_alloc();
+
+    // set the general service ID
+    sdp_uuid128_create( &svc_uuid, &service_uuid_int );
+    sdp_set_service_id( record, svc_uuid );
+
+    // make the service record publicly browsable
+    sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
+    root_list = sdp_list_append(0, &root_uuid);
+    sdp_set_browse_groups( record, root_list );
+
+    // set l2cap information
+    sdp_uuid16_create(&l2cap_uuid, L2CAP_UUID);
+    l2cap_list = sdp_list_append( 0, &l2cap_uuid );
+    proto_list = sdp_list_append( 0, l2cap_list );
+
+    // set rfcomm information
+    sdp_uuid16_create(&rfcomm_uuid, RFCOMM_UUID);
+    channel = sdp_data_alloc(SDP_UINT8, &rfcomm_channel);
+    rfcomm_list = sdp_list_append( 0, &rfcomm_uuid );
+    sdp_list_append( rfcomm_list, channel );
+    sdp_list_append( proto_list, rfcomm_list );
+
+    // attach protocol information to service record
+    access_proto_list = sdp_list_append( 0, proto_list );
+    sdp_set_access_protos( record, access_proto_list );
+
+    // set the name, provider, and description
+    sdp_set_info_attr(record, service_name, service_prov, service_dsc);
+    int err = 0;
+    sdp_session_t *session = 0;
+
+    // connect to the local SDP server, register the service record, and 
+    // disconnect
+    session = sdp_connect( BDADDR_ANY, BDADDR_LOCAL, SDP_RETRY_IF_BUSY );
+    err = sdp_record_register(session, record, 0);
+
+    // cleanup
+    sdp_data_free( channel );
+    sdp_list_free( l2cap_list, 0 );
+    sdp_list_free( rfcomm_list, 0 );
+    sdp_list_free( root_list, 0 );
+    sdp_list_free( access_proto_list, 0 );
+    return session;
+
+	}
+
+
+int init_server() {
+	int port = 3, result, sock, client, bytes_read, bytes_sent;
+	struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
+	char buffer[1024] = { 0 };
+	socklen_t opt = sizeof(rem_addr);
+
+	// local bluetooth adapter
+	loc_addr.rc_family = AF_BLUETOOTH;
+	loc_addr.rc_bdaddr = *BDADDR_ANY;
+	loc_addr.rc_channel = (uint8_t) port;
+
+	// register service
+	sdp_session_t *session = register_service();
+
+	// allocate socket
+	sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+	printf("socket() returned %d\n", sock);
+
+	// bind socket to port 3 of the first available
+	result = bind(sock, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+	printf("bind() on channel %d returned %d\n", port, result);
+
+	// put socket into listening mode
+	result = listen(sock, 1);
+	printf("listen() returned %d\n", result);
+
+	//sdpRegisterL2cap(port);
+
+	// accept one connection
+	printf("calling accept()\n");
+	client = accept(sock, (struct sockaddr *)&rem_addr, &opt);
+	printf("accept() returned %d\n", client);
+
+	ba2str(&rem_addr.rc_bdaddr, buffer);
+	fprintf(stderr, "accepted connection from %s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
+
+	return client;
+}
+
+char *read_server(int client) {
+	// read data from the client
+	char input[1024] = { 0 };
+	int bytes_read;
+	bytes_read = read(client, input, sizeof(input));
+	if (bytes_read > 0) {
+		printf("received [%s]\n", input);
+		return input;
+	} else {
+		return "";
+	}
+}
+
+void write_server(int client, char *message) {
+	// send data to the client
+	char messageArr[1024] = { 0 };
+	int bytes_sent;
+	sprintf(messageArr, message);
+	bytes_sent = write(client, messageArr, sizeof(messageArr));
+	if (bytes_sent > 0) {
+		printf("sent [%s]\n", messageArr);
+	}
+}
+
 
