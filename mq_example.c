bd5758d0b8ad980493b7ca14ce4c5eda4c844cc8
Kiran Joy <kijoyin@gmail.com>
2018-09-16 01:30:56 +0000

    first message received

--- mq_example.c
+++ mq_example.c
@@ -44,7 +44,7 @@ static float speedKmPerHour;
 
 void calculateSpeed_main(void);
 void receiveCarCmd_main(void);
-sdp_session_t *register_service();
+sdp_session_t *register_service(uint8_t rfcomm_channel);
 int init_server();
 
 
@@ -153,74 +153,97 @@ void calculateSpeed_main(void) {
         usleep(exec_period_usecs);
     }
 }
-sdp_session_t *register_service() {
-    
-	uint32_t service_uuid_int[] = { 0, 0, 0, 0xABCD };
-    uint8_t rfcomm_channel = 11;
-    const char *service_name = "Roto-Rooter Data Router";
-    const char *service_dsc = "An experimental plumbing router";
-    const char *service_prov = "Roto-Rooter";
+sdp_session_t *register_service(uint8_t rfcomm_channel) {
 
-    uuid_t root_uuid, l2cap_uuid, rfcomm_uuid, svc_uuid;
-    sdp_list_t *l2cap_list = 0, 
-               *rfcomm_list = 0,
-               *root_list = 0,
-               *proto_list = 0, 
-               *access_proto_list = 0;
-    sdp_data_t *channel = 0, *psm = 0;
+	/* A 128-bit number used to identify this service. The words are ordered from most to least
+	* significant, but within each word, the octets are ordered from least to most significant.
+	* For example, the UUID represneted by this array is 00001101-0000-1000-8000-00805F9B34FB. (The
+	* hyphenation is a convention specified by the Service Discovery Protocol of the Bluetooth Core
+	* Specification, but is not particularly important for this program.)
+	*
+	* This UUID is the Bluetooth Base UUID and is commonly used for simple Bluetooth applications.
+	* Regardless of the UUID used, it must match the one that the Armatus Android app is searching
+	* for.
+	*/
+	uint32_t svc_uuid_int[] = { 0x01110000, 0x00100000, 0x80000080, 0xFB349B5F };
+	const char *service_name = "Armatus Bluetooth server";
+	const char *svc_dsc = "A HERMIT server that interfaces with the Armatus Android app";
+	const char *service_prov = "Armatus";
 
-    sdp_record_t *record = sdp_record_alloc();
+	uuid_t root_uuid, l2cap_uuid, rfcomm_uuid, svc_uuid,
+	       svc_class_uuid;
+	sdp_list_t *l2cap_list = 0,
+	            *rfcomm_list = 0,
+	             *root_list = 0,
+	              *proto_list = 0,
+	               *access_proto_list = 0,
+	                *svc_class_list = 0,
+	                 *profile_list = 0;
+	sdp_data_t *channel = 0;
+	sdp_profile_desc_t profile;
+	sdp_record_t record = { 0 };
+	sdp_session_t *session = 0;
 
-    // set the general service ID
-    sdp_uuid128_create( &svc_uuid, &service_uuid_int );
-    sdp_set_service_id( record, svc_uuid );
+	// set the general service ID
+	sdp_uuid128_create(&svc_uuid, &svc_uuid_int);
+	sdp_set_service_id(&record, svc_uuid);
 
-    // make the service record publicly browsable
-    sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
-    root_list = sdp_list_append(0, &root_uuid);
-    sdp_set_browse_groups( record, root_list );
+	char str[256] = "";
+	sdp_uuid2strn(&svc_uuid, str, 256);
+	printf("Registering UUID %s\n", str);
 
-    // set l2cap information
-    sdp_uuid16_create(&l2cap_uuid, L2CAP_UUID);
-    l2cap_list = sdp_list_append( 0, &l2cap_uuid );
-    proto_list = sdp_list_append( 0, l2cap_list );
+	// set the service class
+	sdp_uuid16_create(&svc_class_uuid, SERIAL_PORT_SVCLASS_ID);
+	svc_class_list = sdp_list_append(0, &svc_class_uuid);
+	sdp_set_service_classes(&record, svc_class_list);
 
-    // set rfcomm information
-    sdp_uuid16_create(&rfcomm_uuid, RFCOMM_UUID);
-    channel = sdp_data_alloc(SDP_UINT8, &rfcomm_channel);
-    rfcomm_list = sdp_list_append( 0, &rfcomm_uuid );
-    sdp_list_append( rfcomm_list, channel );
-    sdp_list_append( proto_list, rfcomm_list );
+	// set the Bluetooth profile information
+	sdp_uuid16_create(&profile.uuid, SERIAL_PORT_PROFILE_ID);
+	profile.version = 0x0100;
+	profile_list = sdp_list_append(0, &profile);
+	sdp_set_profile_descs(&record, profile_list);
 
-    // attach protocol information to service record
-    access_proto_list = sdp_list_append( 0, proto_list );
-    sdp_set_access_protos( record, access_proto_list );
+	// make the service record publicly browsable
+	sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
+	root_list = sdp_list_append(0, &root_uuid);
+	sdp_set_browse_groups(&record, root_list);
 
-    // set the name, provider, and description
-    sdp_set_info_attr(record, service_name, service_prov, service_dsc);
-    int err = 0;
-    sdp_session_t *session = (sdp_session_t*)malloc(sizeof(sdp_session_t));
-    printf("Value: %p\n", session );
-    // connect to the local SDP server, register the service record, and 
-    // disconnect
-    // https://raspberrypi.stackexchange.com/questions/41776/failed-to-connect-to-sdp-server-on-ffffff000000-no-such-file-or-directory
-    // run this sudo chmod 777 /var/run/sdp
-    session = sdp_connect( BDADDR_ANY, BDADDR_LOCAL, SDP_RETRY_IF_BUSY );
-    printf("Value: %d\n", *session );
-    printf("Value: %p\n", session );
-    printf("Value: %p\n", &session );
-    err = sdp_record_register(session, record, 0);
+	// set l2cap information
+	sdp_uuid16_create(&l2cap_uuid, L2CAP_UUID);
+	l2cap_list = sdp_list_append(0, &l2cap_uuid);
+	proto_list = sdp_list_append(0, l2cap_list);
 
-    // cleanup
-    sdp_data_free( channel );
-    sdp_list_free( l2cap_list, 0 );
-    sdp_list_free( rfcomm_list, 0 );
-    sdp_list_free( root_list, 0 );
-    sdp_list_free( access_proto_list, 0 );
-    return session;
+	// register the RFCOMM channel for RFCOMM sockets
+	sdp_uuid16_create(&rfcomm_uuid, RFCOMM_UUID);
+	channel = sdp_data_alloc(SDP_UINT8, &rfcomm_channel);
+	rfcomm_list = sdp_list_append(0, &rfcomm_uuid);
+	sdp_list_append(rfcomm_list, channel);
+	sdp_list_append(proto_list, rfcomm_list);
 
-	}
+	access_proto_list = sdp_list_append(0, proto_list);
+	sdp_set_access_protos(&record, access_proto_list);
 
+	// set the name, provider, and description
+	sdp_set_info_attr(&record, service_name, service_prov, svc_dsc);
+
+	// connect to the local SDP server, register the service record,
+	// and disconnect
+	//sudo chmod 777 /var/run/sdp
+	//https://askubuntu.com/questions/775303/unified-remote-bluetooth-could-not-connect-to-sdp
+	session = sdp_connect(BDADDR_ANY, BDADDR_LOCAL, SDP_RETRY_IF_BUSY);
+	sdp_record_register(session, &record, 0);
+
+	// cleanup
+	sdp_data_free(channel);
+	sdp_list_free(l2cap_list, 0);
+	sdp_list_free(rfcomm_list, 0);
+	sdp_list_free(root_list, 0);
+	sdp_list_free(access_proto_list, 0);
+	sdp_list_free(svc_class_list, 0);
+	sdp_list_free(profile_list, 0);
+
+	return session;
+}
 
 int init_server() {
 	int port = 3, result, sock, client, bytes_read, bytes_sent;
@@ -234,7 +257,7 @@ int init_server() {
 	loc_addr.rc_channel = (uint8_t) port;
 
 	// register service
-	sdp_session_t* session = register_service();
+	sdp_session_t *session = register_service(port);
 
 	// allocate socket
 	sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
@@ -258,6 +281,12 @@ int init_server() {
 	ba2str(&rem_addr.rc_bdaddr, buffer);
 	fprintf(stderr, "accepted connection from %s\n", buffer);
 	memset(buffer, 0, sizeof(buffer));
+	
+	// read data from the client
+    bytes_read = read(client, buffer, sizeof(buffer));
+    if( bytes_read > 0 ) {
+        printf("received [%s]\n", buffer);
+    }
 
 	return client;
 }
